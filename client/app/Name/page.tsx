4acc2becac747e6e0bf1a0b767c2dc81754de324
yash <yashwable12@gmail.com>
2023-11-21 00:49:15 +0530

    Fix formatting and remove unnecessary code

--- client/app/Name/page.tsx
+++ client/app/Name/page.tsx
@@ -1,36 +1,52 @@
-import '../styles/globals.css'
-import 'animate.css';
-import { Roboto } from 'next/font/google'
-import name from '../styles/name.module.css'
+import "../styles/globals.css";
+import "animate.css";
+import { Roboto } from "next/font/google";
+import name from "../styles/name.module.css";
 import mpage from "../styles/MainPage.module.css";
-import Image from 'next/image';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Middle() {
-    return (
-        <>
-          
-        <div>
-          <div className={name.logo}> 
+  return (
+    <>
+      <div>
+        <div className={name.logo}>
           <Image
             src="/logo2.png"
             alt="Description of the image"
             width={235}
             height={300}
-            />
-            </div>
-            <div className={name.fstyle}>
-                <h1 className="animate__animated animate__backInDown"> E-waste Recycling</h1>
-            </div>
-            <div className={mpage.buttonbody}>
-            <button className={mpage.cssbutton}> Get started
-            <div className={mpage.icon}>
-            <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
-            </div>
+          />
+        </div>
+        <div className={name.fstyle}>
+          <h1 className="animate__animated animate__backInDown">
+            {" "}
+            E-waste Recycling
+          </h1>
+        </div>
+        <div className={mpage.buttonbody}>
+          <Link href="/userSignUp">
+            <button className={mpage.cssbutton}>
+              {" "}
+              Get started
+              <div className={mpage.icon}>
+                <svg
+                  height="24"
+                  width="24"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M0 0h24v24H0z" fill="none"></path>
+                  <path
+                    d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
             </button>
-            </div>
-          </div>
-        </>
-    )}
-
-
-    
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
--- client/app/collectorSignUp/form.tsx
+++ client/app/collectorSignUp/form.tsx
@@ -80,7 +80,7 @@ export default function Form() {
                         name="contactPerson"
                         id="contactPerson"
                         autoComplete="contactPerson"
-                        className="pl-1 block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                         placeholder="janesmith"
                       />
                     </div>
