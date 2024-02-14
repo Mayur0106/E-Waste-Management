e6f3d09d4ff11e86b4895cfc6a3d96c1423a354f
yash <yashwable12@gmail.com>
2024-02-15 00:40:15 +0530

    updated user profile and collector filter

--- client/app/Header/page.tsx
+++ client/app/Header/page.tsx
@@ -11,12 +11,12 @@ import { useRouter } from "next/navigation";
 //import slider from './slider';
 
 const coinCount = 16;
+const User = JSON.parse(localStorage.getItem("user") || "");
 
 const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  name: User.userName,
+  email: User.email,
+  imageUrl: `${process.env.NEXT_PUBLIC_REACT_APP_BACKEND_URL}/${User.profileImage}`,
 };
 
 const navigation = [
@@ -115,10 +115,10 @@ export default function Header() {
                             <span className="absolute -inset-1.5" />
                             <span className="sr-only">Open user menu</span>
                             <img
-                              loading="lazy"
+                              // loading="lazy"
                               // onClick={handleClick}
                               className="h-8 w-8 rounded-full"
-                              src={user.imageUrl}
+                              src={`${process.env.NEXT_PUBLIC_REACT_APP_BACKEND_URL}/${User.profileImage}`}
                               alt=""
                             />
                           </Menu.Button>
---
+++ client/app/collector/customMarker.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React, { useState, useRef, useEffect } from "react";
+import { Marker, Popup } from "react-leaflet";
+
+const CustomMarker = ({ isActive, data, map }) => {
+  const [refReady, setRefReady] = useState(false);
+  let popupRef = useRef();
+
+  useEffect(() => {
+    if (refReady && isActive) {
+      popupRef.openOn(map);
+    }
+  }, [isActive, refReady, map]);
+
+  return (
+    <Marker position={data.position}>
+      <Popup
+        ref={(r) => {
+          popupRef = r;
+          setRefReady(true);
+        }}
+      >
+        Yupperz
+      </Popup>
+    </Marker>
+  );
+};
+
+export default CustomMarker;
